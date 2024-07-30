dc3f1e642d49cff683b9d6b0a3227faee5a13400
Boiko Yuliia <boyuliafr@gmail.com>
2024-07-30 16:46:46 +0200

    magaz

--- Evaluation/Presentation_TB/assets/js/scriptex4.js
+++ Evaluation/Presentation_TB/assets/js/scriptex4.js
@@ -1,40 +1,71 @@
-let pu;
-function calculate(pu) {
-   let qtecom = prompt("Entrez quantité");
+let i = 0;
+let tot = 0;
 
-   function pay(pu, qtecom) {
-       let tot = pu * qtecom;
-       let port = 0;
-       let rem = 0;
+function ajout(button, x) {
+    let parentDiv = button.parentElement;
+    let textElement = parentDiv.querySelector('p');
+    let text = textElement.innerHTML.trim(); // Используем innerHTML для сохранения форматирования
+    let parent = document.getElementById('panier');
+    let newDiv = document.createElement('div');
+    
+    // Добавляем текст содержимого элемента 'p' в новый 'div'
+    newDiv.innerHTML = text;
 
-       if (tot >= 100) {
-           if (tot <= 200) {
-               rem = tot * 0.05; 
-           } else {
-               rem = tot * 0.10; 
-           }
-       } else {
-           rem = 0;
-       }
+    // Вставляем новый 'div' в нужное место
+    if (parent.children.length >= 2) {
+        parent.insertBefore(newDiv, parent.children[1]);
+    } else {
+        parent.appendChild(newDiv);
+    }
 
-       tot -= rem; 
+    let inp = parentDiv.querySelector('input[type="number"]');
+    let qtecom = inp.value;
+    let pu = x;
 
-       if (tot > 500) {
-           port = 0;
-       } else {
-           port = tot * 0.02; 
-           if (port < 6) {
-               port = 6; 
-           }
-       }
+    calculate(pu, qtecom);
+}
 
-       tot += port; 
+function calculate(pu, qtecom) {
+    i++;
 
-       
-       document.getElementById("remise").innerHTML = "Remise sur votre commande: " + rem.toFixed(2) + " €";
-       document.getElementById("port").innerHTML = "Livraison: " + port.toFixed(2) + " €";
-       document.getElementById("total").innerHTML = "PAP: " + tot.toFixed(2) + " €";
-   }
+    let qte = parseInt(qtecom, 10);
+    if (isNaN(qte) || qte <= 0) {
+        alert("Quantité non valide!");
+        return;
+    }
 
-   pay(pu, qtecom);
+    let subtotal = pu * qte;
+
+    if (i === 1) {
+        tot = 0;
+    }
+
+    tot += subtotal;
+
+    let rem = 0;
+    if (tot >= 100) {
+        if (tot <= 200) {
+            rem = tot * 0.05;
+        } else {
+            rem = tot * 0.10;
+        }
+    }
+
+    tot -= rem;
+
+    let port = 0;
+    if (tot > 500) {
+        port = 0;
+    } else {
+        port = tot * 0.02;
+        if (port < 6) {
+            port = 6;
+        }
+    }
+
+    tot += port;
+
+    document.getElementById("remise").innerHTML = "Remise sur votre commande: " + rem.toFixed(2) + " €";
+    document.getElementById("port").innerHTML = "Livraison: " + port.toFixed(2) + " €";
+    document.getElementById("total").innerHTML = "PAP: " + tot.toFixed(2) + " €";
 }
