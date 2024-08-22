773cc5b26f3afc8ceed9df2c398f3857fde19afe
Boiko Yuliia <boyuliafr@gmail.com>
2024-08-22 16:46:28 +0200

    cafard

--- MS/assets/js/script.js
+++ MS/assets/js/script.js
@@ -1,40 +1,12 @@
-function calculate() {
-   let pu = document.getElementById("prix").value;
-   let qtecom = document.getElementById("quan").value;
+const carouselWrapper = document.querySelector('.carousel-coverflow-wrapper');
+let angle = 0;
 
-   function pay(pu, qtecom) {
-       let tot = pu * qtecom;
-       let port = 0;
-       let rem = 0;
+document.querySelector('.carousel-control-next').addEventListener('click', () => {
+    angle -= 60;
+    carouselWrapper.style.transform = `rotateY(${angle}deg)`;
+});
 
-       if (tot >= 100) {
-           if (tot <= 200) {
-               rem = tot * 0.05; 
-           } else {
-               rem = tot * 0.10; 
-           }
-       } else {
-           rem = 0;
-       }
-
-       tot -= rem; 
-
-       if (tot > 500) {
-           port = 0;
-       } else {
-           port = tot * 0.02; 
-           if (port < 6) {
-               port = 6; 
-           }
-       }
-
-       tot += port; 
-
-       
-       document.getElementById("remise").innerHTML = "Remise sur votre commande: " + rem.toFixed(2) + " €";
-       document.getElementById("port").innerHTML = "Livraison: " + port.toFixed(2) + " €";
-       document.getElementById("total").innerHTML = "PAP: " + tot.toFixed(2) + " €";
-   }
-
-   pay(pu, qtecom);
-}
+document.querySelector('.carousel-control-prev').addEventListener('click', () => {
+    angle += 60;
+    carouselWrapper.style.transform = `rotateY(${angle}deg)`;
+});
